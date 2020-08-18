61bad8d01b096cc43f6cd6b2a1196ecd972ea606
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-18 15:03:55 +0700

    GraphQL backend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const path = require('path');
+const schema = require('./data/schema');
+const resolvers = require('./data/resolvers');
+
+const APP_PORT = 3000;
+
+const app = express();
+
+// Serve static resources
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+
+// Setup GraphQL server
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    pretty: true,
+    graphiql: true,
+  }),
+);
+
+app.listen(APP_PORT, () => {
+  console.log(`App is now running on http://localhost:${APP_PORT}`);
+});
